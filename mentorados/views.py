e1bf4fa446e8593a6b41fa9798723c958628b842
ZumbiBaiano <isaacbarros709@gmail.com>
2025-04-06 01:10:25 -0300

    alteracao 3

--- mentorados/views.py
+++ mentorados/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Mentorados, Navigators
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def mentorados(request):
-    return HttpResponse('Estou no mentorados')
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    if request.method == 'GET':
+        navigators = Navigators.objects.filter(user=request.user)
+        mentorados = Mentorados.objects.filter(user=request.user)
+        print(mentorados)
+        return render(request, 'mentorados.html', {'estagios': Mentorados.
+        estagio_choices, 'navigators':navigators,'mentorados':mentorados })
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        foto = request.FILES.get('foto')
+        estagio = request.POST.get("estagio")
+        navigator = request.POST.get('navigator')
+
+        mentorado = Mentorados(
+            nome=nome,
+            foto=foto,
+            estagio=estagio,
+            navigator_id=navigator,
+            user=request.user
+
+        )
+
+        mentorado.save()
+
+        messages.add_message(request, constants.SUCCESS,'Mentorado cadastrado com sucesso.')
+        return redirect('mentorados')
+
+
+
+
